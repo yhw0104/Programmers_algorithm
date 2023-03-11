28ce422b300d4b5a11aec194156f067d34910dac
yhw0104 <yhw0104@naver.com>
2023-03-12 01:08:05 +0900

    Division

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        System.out.println(A / B);
+    }
+}
