323e51b292ad93b0f8b9d5fc1e7a7ee15ae33e7f
yhw0104 <yhw0104@naver.com>
2023-03-12 01:25:57 +0900

    Years

---
+++ Years.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Years {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        System.out.println(y-543);
+    }
+}
