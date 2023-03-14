2ff223a1b9c855caf5b68fd06a3cefd1768af95b
yhw0104 <yhw0104@naver.com>
2023-03-14 16:05:56 +0900

    LeapYear

---
+++ Compare.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Compare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A > B){
+            System.out.println(">");
+        }
+
+        if(A < B){
+            System.out.println("<");
+        }
+
+        if(A == B){
+            System.out.println("==");
+        }
+    }
+}
